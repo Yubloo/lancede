160f4e96f703c9df80859a81d8775dd204c5769f
Rémi FLAMENT <remi.flament.78@gmail.com>
2020-12-14 12:50:58 +0100

    simplify encore

--- de.py
+++ de.py
@@ -9,11 +9,12 @@ nbdelancplayer = input("Combien voulez vous faire de lancés de 2 dés: ")
 while nbdelanc < int(nbdelancplayer):
     de1 = random.randint(0, 6)
     de2 = random.randint(0, 6)
+    result = de1 + de2
     nbdelanc += 1
-    if de1 + de2 == 7:
+    if result == 7:
         nbde7 += 1
         print("7")
     else:
-        print(de1 + de2)
+        print(result)
 
 print("Vérification terminée:\nPour ", nbdelancplayer, " lancé le chiffre 7 resort ", nbde7, " fois")
